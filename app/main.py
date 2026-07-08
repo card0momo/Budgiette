fb71732ea536b59c38fb45ecee1b4e2d5b1a6710
Cardomomo <grm-5@hotmail.com>
2026-07-07 20:10:10 -0600

    Enable credentials for localhost CORS during testing

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ def create_application() -> FastAPI:
     application.add_middleware(
         CORSMiddleware,
         allow_origins=settings.cors_allow_origins,
-        allow_credentials=False,
+        allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
